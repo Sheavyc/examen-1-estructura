96c3187bd0d0c4a53cc6cf6a24567d48c11c7e36
Sheavyc <scerdas119100454@gmail.com>
2024-02-27 21:36:55 -0600

    Update Lista

---
+++ Lista.java
@@ -0,0 +1,15 @@
+//import javax.swing.JOptionPane;
+public class Lista {
+    Nodo Cabeza; //Nodo Cabeza de la lista
+    
+    public Lista()
+    {
+        this.Cabeza = null;
+    }
+    
+    //metodos
+    public void agregarProducto(){};
+    public void editarPedido(){};
+    public void eliminarPedido(){};
+    public void mostrarPedido(){};
+}
